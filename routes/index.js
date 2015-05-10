835aa0201405a5206e254bb6aac9cebbc1ae69b0
Kim Nobay <kim@nobay.co.uk>
2015-05-10 23:03:28 +0100

    Got fixtures in a separate javascript file finally!

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var fixtures= require("myscripts/fixtures.js");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', author: 'Yodelly' });
+  res.render('index', { title: 'Fixture app', fixturelist:fixtures.getFixtures()});
 });
 
 module.exports = router;
